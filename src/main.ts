ee95096a4283aa1265d338cf2ceafec628da57e4
Alex Chipre <alechipre@gmail.com>
2024-05-23 16:51:44 -0500

    add Style in the calc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
-import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    Initial Project
-  </div>
-`
+// Animation Github
+const linkGithub = document.querySelector<HTMLImageElement>('#linkGithub')
+const logoGithub = document.querySelector<HTMLImageElement>('#logoGithub')
+const textGithub = document.querySelector<HTMLImageElement>('#textGithub')
+function showText() {
+  textGithub?.animate([
+    { transform: "translateX(-100%)" },
+  ],{
+    duration: 1000,
+    fill: "both"
+  });
+}
+function hiddenText() {
+  textGithub?.animate([
+    { transform: "translateX(0%)" },
+  ],{
+    duration: 1000,
+    fill: "both"
+  });
+}
+linkGithub?.addEventListener('mouseenter', showText)
+linkGithub?.addEventListener('mouseleave', hiddenText)
 
+//define inputs
+const inputBottom = document.querySelector<HTMLInputElement>('#inputBottom')!
+
+// define number 
+const cero = document.querySelector<HTMLButtonElement>('#C')
+
+
+cero?.addEventListener('click', ()=> {
+  // const value: string = '0'
+  return inputBottom.value = "0"
+})
